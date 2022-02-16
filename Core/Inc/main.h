b15de9faf253dd66203c12888e633c0bbf4ed60b
Anatoly Politsin <politsin@gmail.com>
2022-02-16 23:10:10 +0300

    fix IDE include

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -53,7 +53,7 @@ extern "C" {
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
-
+void trans_to_usart1(char *buf);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
